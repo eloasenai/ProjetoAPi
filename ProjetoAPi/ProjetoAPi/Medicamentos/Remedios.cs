ff8aa04b7e028b1c5dac8b8a0cff3dd3e55f0713
eloa <francodosreissilvae@gmail.com>
2025-02-11 16:47:05 -0300

    criando um arquivo

---
+++ ProjetoAPi/ProjetoAPi/Medicamentos/Remedios.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjetoAPi.Medicamentos
+{
+    public class Remedios
+    {
+        public Guid RemediosId { get; set; }
+        public string Nome { get; set; }
+        public decimal Valor { get; set; }
+        public int Classificacao { get; set; }
+        public string serve { get; set; }
+
+    }
+}
