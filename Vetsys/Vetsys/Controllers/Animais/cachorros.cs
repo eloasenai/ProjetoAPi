9dc36db06bd6336815285eed6f560ed3332ef676
Pietra <pgodoifelipe@gmail.com>
2025-02-11 16:47:28 -0300

    adicionando pasta "animais" de pietra

---
+++ Vetsys/Vetsys/Controllers/Animais/cachorros.cs
@@ -0,0 +1,11 @@
+﻿namespace Vetsys.Controllers.Animais
+{
+    public class cachorros
+    {
+        public string Raca { get; set; }
+        public double Peso { get; set; }
+        public string Pelagem { get; set; }
+        public string Tamanho { get; set; }
+        public int Idade { get; set; }
+    }
+}
