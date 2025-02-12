2713ab946bd198498b4ea3591a13518fda546ecc
Bianca <bianca.furlanetto440@gmail.com>
2025-02-12 09:04:42 -0300

    Criado da classe Animal

---
+++ ProjetoAPi/ProjetoAPi/Models/Animal.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjetoAPi.Models
+{
+    public class Animal
+    {
+        public Guid AnimalId { get; set; }
+        public string Tipo { get; set; }
+        public string Raca { get; set; }
+        public double Peso { get; set; }
+        public string Pelagem { get; set; }
+        public string Tamanho { get; set; }
+        public int Idade { get; set; }
+    }
+}
