2337390d2a834737bd2c9d18e00d10723eeac7e4
Pietra <pgodoifelipe@gmail.com>
2025-02-12 09:08:43 -0300

    Criado a classe Horario

---
+++ ProjetoAPi/ProjetoAPi/Models/Horario.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjetoAPi.Models
+{
+    public class Horario
+    {
+        public Guid HorarioId { get; set; }
+        public Guid AnimalId { get; set; }
+        public Animal? Animal { get; set; }
+        public DateTime Data { get; set; }
+
+    }
+}
