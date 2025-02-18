a583d04fcce5f5b9476467b3550e4e4613ab2e30
eloa <francodosreissilvae@gmail.com>
2025-02-18 11:13:21 -0300

    modificação das models Eloá

--- ProjetoAPi/ProjetoAPi/Models/Cliente.cs
+++ ProjetoAPi/ProjetoAPi/Models/Cliente.cs
@@ -1,13 +1,12 @@
 ﻿namespace ProjetoAPi.Models
 {
-    public class Animal
+    public class Cliente
     {
-        public Guid AnimalId { get; set; }
+        public Guid ClienteId { get; set; }
         public string nome { get; set; }
-        public string especie { get; set; }
-        public int idade { get; set; }
+        public string endereço { get; set; }
+        public string telefone { get; set; }
         public string cadastrar { get; set; }
         public string atualizar { get; set; }
-
     }
 }
---
+++ ProjetoAPi/ProjetoAPi/Models/Servico.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjetoAPi
+{
+    public class Servico
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Duracao { get; set; }
+        public string Agendar { get; set; }
+        public string Cancelar { get; set; }
+    }
+}
